6f0895498b76689bff0afa342b7bea8c133bd543
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2023-11-30 04:12:06 -0500

    first significant update in a while

---
+++ scripts.js
@@ -0,0 +1,12 @@
+/*
+
+JavaScript functionality for the website
+
+Last updated: 2023-11-30
+
+*/
+
+// toggle between showing/hiding an element
+function showhide(id) {
+    document.getElementById(id).classList.toggle('hide');
+}
