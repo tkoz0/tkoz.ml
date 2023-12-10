24e7b24d52b52d7f825913e70456274143199364
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2023-12-09 22:32:52 -0500

    mathjax

--- install-mathjax.sh
+++ install-mathjax.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo runuser -u www-data -- git clone https://github.com/mathjax/MathJax.git mathjax
+sudo runuser -u www-data -- git clone https://github.com/mathjax/MathJax.git mathjax_tmp
+sudo runuser -u www-data -- mkdir mathjax
+sudo runuser -u www-data -- mv mathjax_tmp/es5/* mathjax/
+sudo runuser -u www-data -- rm -rf mathjax_tmp
