857eaafe44e114caed1fb5dc06ae7763c976eb45
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2023-12-16 21:09:20 -0500

    remove mathjax (instead use it where I need it)

--- install-mathjax.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo runuser -u www-data -- git clone https://github.com/mathjax/MathJax.git mathjax_tmp
-sudo runuser -u www-data -- mkdir mathjax
-sudo runuser -u www-data -- mv mathjax_tmp/es5/* mathjax/
-sudo runuser -u www-data -- rm -rf mathjax_tmp
-sudo runuser -u www-data -- cp mathjax_config.js mathjax/config.js
